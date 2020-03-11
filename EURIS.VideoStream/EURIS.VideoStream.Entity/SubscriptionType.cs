4fbf10e2f700b33659a85fc4133614f0c3b48daa
chiliveru <manirathnam.chiliveru@euris.it>
2020-03-11 16:58:16 +0100

    adding comments in subscription type

--- EURIS.VideoStream/EURIS.VideoStream.Entity/SubscriptionType.cs
+++ EURIS.VideoStream/EURIS.VideoStream.Entity/SubscriptionType.cs
@@ -8,10 +8,12 @@ namespace EURIS.VideoStream.Entity
 {
     public class SubscriptionType
     {
+        //Scalar properties
         public Guid SubscriptionTypeId { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
 
+        //Navigation properties
         public List<UserProfile> UserProfile { get; set; }
         public Subscription Subscripiton { get; set; }
     }
