686f9cb9994e4fa5f87b7509da3ac5f7fb799efd
Jabez <manirathnam133@gmail.com>
2020-03-30 16:54:48 +0200

    Test from git bash

--- EURIS.VideoStream/EURIS.VideoStream.Interfaces/IRepository.cs
+++ EURIS.VideoStream/EURIS.VideoStream.Interfaces/IRepository.cs
@@ -15,6 +15,7 @@ namespace EURIS.VideoStream.Interfaces
         void UpdateFavourite(Favourites entity);
         void DeleteFavourite(Guid favouriteId);
         void SaveFavourite();
+		//Testing the git
     }
 
     public interface IUserAccount
